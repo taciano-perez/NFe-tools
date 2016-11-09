4042e3c5473f0a6068379bd55c0c2e3fdce90bac
Taciano Perez <tdperez@hotmail.com>
2016-11-09 23:10:27 +0000

    created DB and test microservice

--- python/create_db.py
+++ python/create_db.py
@@ -13,7 +13,9 @@ cur = db.cursor()
 # Use all the SQL you like
 cur.execute("CREATE DATABASE mdf;")
 cur.execute("USE mdf;")
-cur.execute("CREATE TABLE consumidor(hash_cpf VARCHAR(256), num_nfs SMALLINT, status_coleta TINYINT, data_ult_coleta DATE);")
+cur.execute("CREATE TABLE consumidor(hash_cpf VARCHAR(256) NOT NULL, num_nfs SMALLINT, status_coleta TINYINT, data_ult_coleta DATE, PRIMARY KEY(hash_cpf));")
+cur.execute("CREATE TABLE nf(hash_cpf VARCHAR(256) NOT NULL, numero VARCHAR(32) NOT NULL, municipio VARCHAR(256), razao_social VARCHAR(512), emissao DATE, PRIMARY KEY (hash_cpf, numero));")
+cur.execute("CREATE TABLE nf_item(hash_cpf VARCHAR(256) NOT NULL, numero VARCHAR(32) NOT NULL, desc VARCHAR(512) NOT NULL, codigo VARCHAR(128), qtd SMALLINT, val_unit DECIMAL(15,2), val_total DECIMAL(15,2) PRIMARY KEY (hash_cpf, numero, desc));")
 print 'DB Creation successful.'
 
 # print all the first cell of all the rows
