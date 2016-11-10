b6301093fe0b40dc71740bf223bb68d43276a7d8
Taciano Perez <tdperez@hotmail.com>
2016-11-10 00:10:58 +0000

    Testing parameters in microservice

--- python/REST_coleta_nf.py
+++ python/REST_coleta_nf.py
@@ -1,10 +1,19 @@
 #! /usr/bin/env python
 from flask import Flask
+from flask import request
+
 app = Flask(__name__)
 
-@app.route("/hello")
-def hello():
-    return "Hello World!"
+@app.route("/coletaNF")
+def coletaNF():
+	user = request.args.get('user', '')
+	password = request.args.get('password', '')
+	nf_count = scrapeNFdata(user, password)
+    return "ColetaNF REST microservice " + user + " " + password + " " + nf_count
 
+def scrapeNFdata(user, password):
+	return 2
+
+	
 if __name__ == "__main__":
     app.run(host= '0.0.0.0')
